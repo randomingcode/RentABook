5b6f8847bf5fc7a5b1971eae4e6ce7f099454e31
berkay <berkayozdemir764@gmail.com>
2024-01-21 21:25:46 +0300

    as

--- RentABook/Migrations/20240121182221_as.Designer.cs
+++ RentABook/Migrations/20240121182221_as.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentABook.Data;
 
@@ -10,9 +11,11 @@ using RentABook.Data;
 namespace RentABook.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240121182221_as")]
+    partial class @as
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
