7d9947b548ee317899389f84dc1e098d041fef4a
Ceren Kaya <cerenkaya1997ck@hotmail.com>
2024-01-21 18:52:23 +0300

    J

--- RentABook/Data/Seed.cs
+++ RentABook/Data/Seed.cs
@@ -24,7 +24,9 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
                             Page = 209,
                             Title = "Things Fall Apart",
-                            Year = 1958
+                            Year = 1958,
+                            PriceId = 2,
+
                         },
                         new Book()
                         {
@@ -35,7 +37,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
                             Page = 209,
                             Title = "Things Fall Apart",
-                            Year = 1958
+                            Year = 1958,
+                            PriceId = 3
                         },
                         new Book()
                         {
@@ -47,7 +50,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
                             Page = 784,
                             Title = "Fairy tales",
-                            Year = 1836
+                            Year = 1836,
+                            PriceId = 4
                         },
                         new Book()
                         {
@@ -58,7 +62,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Divine_Comedy\n",
                             Page = 928,
                             Title = "The Divine Comedy",
-                            Year = 1315
+                            Year = 1315,
+                            PriceId = 5
                         },
                         new Book()
                         {
@@ -69,7 +74,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
                             Page = 160,
                             Title = "The Epic Of Gilgamesh",
-                            Year = -1700
+                            Year = -1700,
+                            PriceId = 6
                         },
                         new Book()
                         {
@@ -80,7 +86,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Book_of_Job\n",
                             Page = 176,
                             Title = "The Book Of Job",
-                            Year = -600
+                            Year = -600,
+                            PriceId = 7
 
                         },
 
@@ -93,7 +100,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/One_Thousand_and_One_Nights\n",
                             Page = 288,
                             Title = "One Thousand and One Nights",
-                            Year = 1200
+                            Year = 1200,
+                            PriceId = 8
                         },
 
                         new Book()
@@ -105,7 +113,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Nj%C3%A1ls_saga\n",
                             Page = 384,
                             Title = "Nj\u00e1l's Saga",
-                            Year = 1350
+                            Year = 1350,
+                            PriceId = 8
                         },
                         new Book()
                         {
@@ -116,7 +125,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Pride_and_Prejudice\n",
                             Page = 226,
                             Title = "Pride and Prejudice",
-                            Year = 1813
+                            Year = 1813,
+                            PriceId = 9
                         },
 
                         new Book()
@@ -128,7 +138,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Le_P%C3%A8re_Goriot\n",
                             Page = 443,
                             Title = "Le P\u00e8re Goriot",
-                            Year = 1835
+                            Year = 1835,
+                            PriceId = 10
                         },
 
                         new Book()
@@ -140,7 +151,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Molloy_(novel)\n",
                             Page = 256,
                             Title = "Molloy, Malone Dies, The Unnamable, the trilogy",
-                            Year = 1952
+                            Year = 1952,
+                            PriceId = 11
                         },
 
                         new Book()
@@ -152,7 +164,8 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/The_Decameron\n",
                             Page = 1024,
                             Title = "The Decameron",
-                            Year = 1351
+                            Year = 1351,
+                            PriceId = 12
                         },
                         new Book()
                         {
@@ -163,12 +176,101 @@ namespace RentABook.Data
                             Link = "https://en.wikipedia.org/wiki/Ficciones\n",
                             Page = 224,
                             Title = "Ficciones",
-                            Year = 1965
+                            Year = 1965,
+                            PriceId= 1,
+                           
                         }
 
+                    });
+                    context.Prices.AddRange(new List<Price>()
+                    {
+                        new Price() {
+                            Id = 1,
+                            OriginalPrice = 10,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
 
+
+                        },
+                            new Price() {
+                            Id = 2,
+                            OriginalPrice = 20,
+                            DiscountRatio = 0.25f,
+                            DiscountedPrice = 8,
+                        },
+
+                            new Price() {
+                            Id = 3,
+                            OriginalPrice = 30,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+                            new Price() {
+                            Id = 4,
+                            OriginalPrice = 50,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+                             new Price() {
+                            Id = 5,
+                            OriginalPrice = 80,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+                            new Price() {
+                            Id = 6,
+                            OriginalPrice = 90,
+                            DiscountRatio = 0.4f,
+                            DiscountedPrice = 8,
+
+
+                        },
+                            new Price() {
+                            Id = 7,
+                            OriginalPrice = 20,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+
+                            new Price() {
+                            Id = 8,
+                            OriginalPrice = 40,
+                            DiscountRatio = 0.25f,
+                            DiscountedPrice = 8,
+                        },
+                            new Price() {
+                            Id = 9,
+                            OriginalPrice = 32,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+                             new Price() {
+                            Id = 10,
+                            OriginalPrice = 28,
+                            DiscountRatio = 0.2f,
+                            DiscountedPrice = 8,
+                        },
+                            new Price() {
+                            Id = 11,
+                            OriginalPrice = 65,
+                            DiscountRatio = 0.4f,
+                            DiscountedPrice = 8,
+                        },
+                            new Price() {
+                            Id = 12,
+                            OriginalPrice = 12,
+                            DiscountRatio = 0.3f,
+                            DiscountedPrice = 8,
+                        },
+                             new Price() {
+                            Id = 13,
+                            OriginalPrice = 45,
+                            DiscountRatio = 0.3f,
+                            DiscountedPrice = 8,
+                        }
 
                     });
+
 
 
 
